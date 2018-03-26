a5ca985a2fe57c50f1e77b2ad7e7240b44004ea7
TheHunterGolden <huntergolden0124@gmail.com>
2018-03-26 08:53:11 -0400

    enemy AI movement and animations + arena

---
+++ Assets/EnemyAttack.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAttack : MonoBehaviour {
+
+    NavMeshAgent agent;
+    public Transform playerLoc;
+    private Animator _animator;
+    private bool inRange;
+    public Transform bulletSpawn;
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        _animator = GetComponentInChildren<Animator>();
+        inRange = false;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.LookAt(playerLoc);
+
+        if (inRange == true)
+        {
+            Attack();
+        }
+        else {
+            chase();
+        }
+	}
+
+    void OnTriggerEnter(Collider col) {
+        if (col.gameObject.tag == "square")
+        {
+            agent.destination = gameObject.transform.position;
+
+            inRange = true;
+            
+        }
+       
+    }
+
+    void OnTriggerExit(Collider col) {
+        if (col.gameObject.tag == "square") {
+            inRange = false;
+            _animator.SetBool("Attacking", false);
+        }
+    }
+
+    void Attack() {
+        _animator.SetBool("Attacking", true);
+         
+    }
+
+    void chase() {
+        agent.destination = playerLoc.position;
+    }
+}
---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour {
+
+    private Animator _animator;
+    NavMeshAgent agent;
+
+    public Transform playerLoc;
+   
+
+    // Use this for initialization
+    void Start () {
+
+        _animator = GetComponentInChildren<Animator>();
+        agent = GetComponentInParent<NavMeshAgent>();
+        agent.destination = playerLoc.position;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        _animator.SetFloat("velZ", agent.velocity.magnitude);
+	}
+    
+   
+}
