7c1a562f0b92456d55757f0185f5d5d5c52f895f
Yixiang Xu <yixiangxu@umass.edu>
2018-03-29 13:30:24 -0400

    Added damage detection for attack cube

---
+++ Assets/DamageDetection.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageDetection : MonoBehaviour {
+    public Animator enemyAnimator;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.name);
+        if(collision.gameObject.tag == "playerAttack")
+        {
+            GetComponent<SphereCollider>().enabled = false;
+            GetComponent<CapsuleCollider>().enabled = false;
+            GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotationZ;
+            enemyAnimator.SetBool("Attacked", true);
+        }
+    }
+}
