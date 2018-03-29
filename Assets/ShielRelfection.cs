fa260ef1a59d4778baf585b67fbc76e09fc78fb8
Yixiang Xu <yixiangxu@umass.edu>
2018-03-29 19:26:29 -0400

    changed bullet to fly through enmey

--- Assets/ShielRelfection.cs
+++ Assets/ShielRelfection.cs
@@ -8,6 +8,7 @@ public class ShielRelfection : MonoBehaviour {
         if(collision.gameObject.tag == "enemyAttack")
         {
             collision.gameObject.tag = "playerAttack";
+            collision.gameObject.layer = 0;
         }
     }
 }
