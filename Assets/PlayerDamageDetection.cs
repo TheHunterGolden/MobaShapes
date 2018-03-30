f285d17b099042889f8be2644f74e85a8ee3bbc6
Yixiang Xu <yixiangxu@umass.edu>
2018-03-30 14:28:06 -0400

    made player invincible and be able to kill enemy when dashing

--- Assets/PlayerDamageDetection.cs
+++ Assets/PlayerDamageDetection.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class PlayerDamageDetection : MonoBehaviour {
     public GameObject gameOver;
+    public SkillUseE abilityE;
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "enemyAttack")
+        if (collision.gameObject.tag == "enemyAttack" && !abilityE.activated)
         {
             gameOver.SetActive(true);
             gameObject.SetActive(false);
