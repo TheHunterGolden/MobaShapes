446abacac65d14fc35bcadbf0ad20e06fdaccb26
Yixiang Xu <yixiangxu@umass.edu>
2018-04-13 14:42:52 -0400

    changed target

--- Assets/Target.cs
+++ Assets/Target.cs
@@ -4,12 +4,9 @@ using UnityEngine;
 
 public class Target : MonoBehaviour {
     public Transform squarePerson;
-    public RectTransform rt;
-    public Vector3 position;
 
     void Start()
     {
-        rt = GetComponent<RectTransform>();
         Cursor.visible = false;
     }
 
@@ -19,11 +16,8 @@ public class Target : MonoBehaviour {
 
         if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit1, 1000))
         {
-            Vector3 adjustedPosition = new Vector3(hit1.point.x, squarePerson.position.y, hit1.point.z);
-            Vector3 screenPoint = Camera.main.WorldToScreenPoint(adjustedPosition);
-            rt.localPosition = new Vector3(screenPoint.x - 390, screenPoint.y - 200, 0);
+            Vector3 adjustedPosition = new Vector3(hit1.point.x, hit1.point.y + 1, hit1.point.z);
+            gameObject.transform.position = adjustedPosition;
         }
-
-        position = Input.mousePosition;
     }
 }
