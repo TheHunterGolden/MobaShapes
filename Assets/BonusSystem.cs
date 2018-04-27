08b0a6c1fb89478734e9f8857705abd1b55aee32
Yixiang Xu <yixiangxu@umass.edu>
2018-04-27 14:32:16 -0400

    added basic bonus system

---
+++ Assets/BonusSystem.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BonusSystem : MonoBehaviour {
+    public int currentBonus;
+
+	// Use this for initialization
+	void Start () {
+        currentBonus = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
